65edc28e5b5fb72b5fa5ea09b418aec378d881c8
Дмитрий Тучашвили <dmitriy.211@mail.ru>
2017-04-16 21:32:45 +0600

    Добавьте файлы проекта.

---
+++ Graphics/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphic
+{
+    public partial class Form1 : Form
+    {
+        //Graphics g;
+        Pen pen = new Pen(Color.Blue, 5);        
+        GraphicsPath g;
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush CoralBrush = new SolidBrush(Color.Coral);
+        SolidBrush whBrush = new SolidBrush(Color.White);
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+
+       /* LinearGradientBrush linGrBrush = new LinearGradientBrush(
+           new Point(0, 10),
+           new Point(200, 10),
+           Color.FromArgb(255, 255, 0, 0),   // Opaque red
+           Color.FromArgb(255, 0, 0, 255));  // Opaque blue*/
+
+
+        public Form1()
+        {
+            InitializeComponent();
+           //g = CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        void DrawStar(PaintEventArgs e, float a, float b, float m)  // a = center x , b = center y, m = size multiplying
+        {
+            g = new GraphicsPath();
+
+            g.AddLine(m * 2 + a, b, m * 4 + a, m * 2 + b);
+            g.AddLine(m * 4 + a, m * 2 + b, m * 6 + a, b);
+            g.AddLine(m * 6 + a, b, m * 6 + a, m * 3 + b);
+            g.AddLine(m * 6 + a, m * 3 + b, m * 8 + a, m * 4 + b);
+            g.AddLine(m * 8 + a, m * 4 + b, m * 6 + a, m * 5 + b);
+            g.AddLine(m * 6 + a, m * 5 + b, m * 6 + a, m * 8 + b);
+            g.AddLine(m * 6 + a, m * 8 + b, m * 4 + a, m * 6 + b);
+            g.AddLine(m * 4 + a, m * 6 + b, m * 2 + a, m * 8 + b);
+            g.AddLine(m * 2 + a, m * 8 + b, m * 2 + a, m * 5 + b);
+            g.AddLine(m * 2 + a, m * 5 + b, a, m * 4 + b);
+            g.AddLine(a, m * 4 + b, m * 2 + a, m * 3 + b);
+            g.AddLine(m * 2 + a, m * 3 + b, m * 2 + a, b);
+            
+            e.Graphics.FillPath(CoralBrush, g);            
+        }
+
+  
+        void DrawShip(PaintEventArgs e, float a, float b, float m)
+        {           
+            e.Graphics.FillEllipse(blueBrush, a, b, 4 * m, 4 * m);
+            e.Graphics.FillEllipse(whBrush, a + m, b + m, 2 * m, 2 * m);
+            e.Graphics.DrawLine(pen, a, b, a + m, b + m);
+            e.Graphics.DrawLine(pen, a + 4 * m, b, a + 3 * m, b + m);
+            e.Graphics.DrawLine(pen, a + 4 * m, b + 4 * m, a + 3 * m, b + 3 * m);
+            e.Graphics.DrawLine(pen, a, b + 4 * m, a + m, b + 3 * m);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < Width; i += 100)
+                for (int j = 0; j < Height; j += 100)
+                    e.Graphics.FillEllipse(whBrush, i, j, 2, 2);
+            for (int i = 50; i < Width; i += 100)
+                for (int j = 50; j < Height; j += 100)
+                    e.Graphics.FillEllipse(whBrush, i, j, 2, 2);
+
+            DrawStar(e, 0, 0, 10);
+            DrawStar(e, 250, 50, 10);
+            DrawStar(e, 180, 200, 10);
+            DrawStar(e, 400, 100, 10);
+            DrawStar(e, 600, 250, 10);
+            DrawStar(e, 700, 0, 10);
+            DrawStar(e, 400, 300, 10);
+
+            Bitmap bitmap = new Bitmap("Bez-imeni-2-_2.gif");  // Раскомменчивать только в крайних случаях
+            e.Graphics.DrawImage(bitmap, 350, 20);
+
+            DrawShip(e, 100, 100, 10);
+        }
+    }
+}
---
+++ Paint/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(pictureBox1.Image);
+            //g = pictureBox1.CreateGraphics();
+        }
+
+        Point prPnt;
+        Graphics g;
+        GraphicsPath gp = new GraphicsPath();
+        Point curPnt;
+        bool endLine = true;
+        Point X;
+        Queue<Point> q = new Queue<Point>();
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            prPnt = e.Location;
+            X = e.Location;
+
+            if (Instrument_box.Text == "Brush")
+                g.FillEllipse(new SolidBrush(color), prPnt.X - width / 2, prPnt.Y - width / 2, width + 2, width + 2);
+
+            if (Instrument_box.Text == "Fill")
+                Fill(e.Location);
+
+            endLine = false;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            g.DrawPath(new Pen(color, width), gp);
+            endLine = true;          
+        }
+
+        
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            gp.Reset();
+
+            if (e.Button == MouseButtons.Left && Instrument_box.Text == "Brush")
+            {
+                curPnt = e.Location;                
+                g.FillEllipse(new SolidBrush(color) , prPnt.X - width/2, prPnt.Y - width/2, width+2, width+2);
+                prPnt = curPnt;
+            }
+
+            if (e.Button == MouseButtons.Left && Instrument_box.Text == "Pen")
+            {
+                curPnt = e.Location;
+                g.DrawLine(new Pen(color, width), prPnt, curPnt);                
+                prPnt = curPnt;
+            }
+            
+            if (Instrument_box.Text == "Line" && endLine == false)
+            {               
+                curPnt = e.Location;
+                gp.AddLine(prPnt, curPnt);                
+            }
+
+            if ((Instrument_box.Text == "Rectangle" || Instrument_box.Text == "Ellipse") && endLine == false)
+            {
+                curPnt = e.Location;
+
+                if (curPnt.X >= prPnt.X && curPnt.Y >= prPnt.Y)
+                {
+                    curPnt = e.Location;
+                    prPnt = X;
+                }                
+
+                if (curPnt.X >= X.X && curPnt.Y <= X.Y)
+                {
+                    prPnt.Y = e.Location.Y;
+                    prPnt.X = X.X;
+                    curPnt.Y = X.Y;
+                    curPnt.X = e.Location.X;
+                }
+
+                if (curPnt.X <= X.X && curPnt.Y >= X.Y)
+                {
+                    prPnt.X = e.Location.X;
+                    prPnt.Y = X.Y;
+                    curPnt.X = X.X;
+                    curPnt.Y = e.Location.Y;
+                }
+
+                if (curPnt.X <= X.X && curPnt.Y <= X.Y)
+                {
+                    curPnt = X;
+                    prPnt = e.Location;
+                }
+
+                if (Instrument_box.Text == "Rectangle")                 
+                    gp.AddRectangle(new Rectangle(prPnt.X, prPnt.Y, curPnt.X - prPnt.X, curPnt.Y - prPnt.Y));
+                if (Instrument_box.Text == "Ellipse")
+                    gp.AddEllipse(new Rectangle(prPnt.X, prPnt.Y, curPnt.X - prPnt.X, curPnt.Y - prPnt.Y));
+            }
+
+
+
+            pictureBox1.Refresh();
+        }
+        Color color = Color.Red;
+        Color colorOrigin;
+        float width;
+
+        private void Clear_btn_Click(object sender, EventArgs e)
+        {
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Refresh();
+        }
+
+        private void Color_btn_Click(object sender, EventArgs e)
+        {            
+            ColorDialog cdlg = new ColorDialog();
+            if (cdlg.ShowDialog() == DialogResult.OK)
+                color = cdlg.Color;
+            Color_btn.BackColor = color;
+        }
+
+        private void Pen_num_ValueChanged(object sender, EventArgs e)
+        {
+            width = (float)Pen_num.Value;            
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {            
+                e.Graphics.DrawPath(new Pen(color, width), gp);            
+        }
+
+        private void Fill(Point mouseLoc)
+        {     
+            colorOrigin = bmp.GetPixel(mouseLoc.X, mouseLoc.Y);
+            if (colorOrigin == color)
+                return;
+            q.Enqueue(mouseLoc);                       
+            while (q.Count > 0)
+            {                
+                Point curPoint = q.Dequeue();
+                Step(curPoint.X + 1, curPoint.Y);
+                Step(curPoint.X - 1, curPoint.Y);
+                Step(curPoint.X, curPoint.Y + 1);
+                Step(curPoint.X, curPoint.Y - 1);
+            }
+            pictureBox1.Refresh();
+
+        }
+
+        private void Step(int x, int y)
+        {
+            if (x < 0) return;
+            if (y < 0) return;
+            if (x >= bmp.Width) return;
+            if (y >= bmp.Height) return;
+            if (bmp.GetPixel(x, y) != colorOrigin) return;
+            bmp.SetPixel(x, y, color);
+            q.Enqueue(new Point(x, y));
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+                pictureBox1.Image.Save(sfd.FileName);
+        }
+    }
+}
